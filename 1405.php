0917603cd867d9fdda706ea55b42f94882043633
brianhartmann09 <hartmannbrian09@gmail.com>
2025-05-14 10:07:04 -0300

    Add files via upload

---
+++ 1405.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+echo "<h1> Mamma ! </h1>";
+$tamanho= "m";
+$bebida= "5.99";
+$preco= $bebida+60;
+echo "<h2> O valor total é $preco </h2>";
+
+?> 
+
+</body>
+</html>
